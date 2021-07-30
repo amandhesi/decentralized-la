1f88459fd1ea25f77c7fb8d7981cffced5506002
amandhesi <amandhesi2@gmail.com>
2021-07-30 15:56:12 -0700

    add welcome message, remove unused files, add fix for untyped typeform library

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { PopupButton } from '@typeform/embed-react'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="About">
+        Decentralized LA is a community of Cryptoasset and Web3.0 builders, entrepreneurs and investors in Los Angeles. We organize curated, invite-only events every month.
+
+        Apply to be part of the community by filling out the form below.
+      </div>
     </div>
   );
 }
